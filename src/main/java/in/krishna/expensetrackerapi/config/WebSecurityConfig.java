d483cd263be7d1cf52c5ae0238d5d52c69953700
krnand <1india.kng@gmail.com>
2024-03-30 12:35:50 +0530

    customizes multiple users using InMemoryUserDetailsManager class

--- src/main/java/in/krishna/expensetrackerapi/config/WebSecurityConfig.java
+++ src/main/java/in/krishna/expensetrackerapi/config/WebSecurityConfig.java
@@ -1,10 +1,15 @@
 package in.krishna.expensetrackerapi.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -19,13 +24,36 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().httpBasic();
     }
 
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("Krishna").password("12345").authorities("User")
+//                .and()
+//                .withUser("Ankit").password("12345").authorities("admin")
+//                .and()
+//                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+//    }
+
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("Krishna").password("12345").authorities("User")
-                .and()
-                .withUser("Ankit").password("12345").authorities("admin")
-                .and()
-                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+        UserDetails userDetails1 = User.withUsername("Krishna").password("12345").authorities("user").build();
+        UserDetails userDetails2 = User.withUsername("ankit").password("12345").authorities("admin").build();
+
+        //create user
+        userDetailsManager.createUser(userDetails1);
+        userDetailsManager.createUser(userDetails2);
+
+        // we need to tell spring security that user has been created for that
+        // we will use AuthenticationManagerBuilder auth
+        auth.userDetailsService(userDetailsManager);
     }
+
+    // also need to provide password encoders
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 }
