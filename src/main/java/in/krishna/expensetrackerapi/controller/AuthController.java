6244c145f01d370f79f4822b102793596eecde43
krnand <1india.kng@gmail.com>
2024-03-31 00:43:02 +0530

    Updated Login Rest API to validate user

--- src/main/java/in/krishna/expensetrackerapi/controller/AuthController.java
+++ src/main/java/in/krishna/expensetrackerapi/controller/AuthController.java
@@ -1,11 +1,17 @@
 package in.krishna.expensetrackerapi.controller;
 
+import in.krishna.expensetrackerapi.entity.AuthModel;
 import in.krishna.expensetrackerapi.entity.User;
 import in.krishna.expensetrackerapi.entity.UserModel;
 import in.krishna.expensetrackerapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,13 +24,19 @@ public class AuthController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @PostMapping("/login")
-    public ResponseEntity<String> login(){
-        return new ResponseEntity<>("User is logged in", HttpStatus.OK);
+    public ResponseEntity<HttpStatus> login(@RequestBody AuthModel authModel) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authModel.getEmail(),
+                authModel.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping("/register")
-    public ResponseEntity<User> saveUser(@Valid @RequestBody UserModel userModel){
+    public ResponseEntity<User> saveUser(@Valid @RequestBody UserModel userModel) {
         return new ResponseEntity<User>(userService.createUser(userModel), HttpStatus.CREATED);
     }
 }
---
+++ src/main/java/in/krishna/expensetrackerapi/entity/AuthModel.java
@@ -0,0 +1,9 @@
+package in.krishna.expensetrackerapi.entity;
+
+import lombok.Data;
+
+@Data
+public class AuthModel {
+    private String email;
+    private String password;
+}
