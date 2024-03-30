b1a2033f0e1c41d20d403f0389e4776ae32989e2
krnand <1india.kng@gmail.com>
2024-03-30 12:05:18 +0530

    added spring security and customizes http request

--- src/main/java/in/krishna/expensetrackerapi/controller/ExpenseController.java
+++ src/main/java/in/krishna/expensetrackerapi/controller/ExpenseController.java
@@ -3,9 +3,10 @@ package in.krishna.expensetrackerapi.controller;
 import in.krishna.expensetrackerapi.entity.Expense;
 import in.krishna.expensetrackerapi.service.ExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -17,5 +18,11 @@ public class ExpenseController {
     @GetMapping("/expenses")
     public List<Expense> getAllExpenses(){
         return expenseService.getAllListOfExpenses();
+    }
+
+    @ResponseStatus(value = HttpStatus.CREATED)
+    @PostMapping("/expenses")
+    public Expense saveExpenseDetails(@Valid @RequestBody Expense expense) {
+        return expenseService.saveExpenseDetails(expense);
     }
 }
--- src/main/java/in/krishna/expensetrackerapi/entity/Expense.java
+++ src/main/java/in/krishna/expensetrackerapi/entity/Expense.java
@@ -1,12 +1,19 @@
 package in.krishna.expensetrackerapi.entity;
 
-import jakarta.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 import java.sql.Date;
+import java.sql.Timestamp;
 
 @Entity
 @AllArgsConstructor
@@ -17,11 +24,30 @@ public class Expense {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "expense_name")
+    @NotBlank(message = "Expense name must not be null")
+    @Size(min = 3, message = "Expense name must be atleast 3 characters")
     private String name;
+
+
     private String description;
+
     @Column(name = "expense_amount")
+    @NotNull(message = "Expense amount should not be null")
     private BigDecimal amount;
+
+    @NotBlank(message = "Category should not be null")
     private String category;
+
+    @NotNull(message = "Date must not be null")
     private Date date;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private Timestamp updatedAt;
 }
