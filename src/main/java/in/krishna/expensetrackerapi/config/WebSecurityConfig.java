c6e09b4d98fe6b4591684227eb167933ac1049c9
krnand <1india.kng@gmail.com>
2024-03-30 12:09:32 +0530

    added customizes multiple users

--- src/main/java/in/krishna/expensetrackerapi/config/WebSecurityConfig.java
+++ src/main/java/in/krishna/expensetrackerapi/config/WebSecurityConfig.java
@@ -10,14 +10,22 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.csrf().disable()
-			.authorizeRequests()
-			.antMatchers("/login", "/register").permitAll()
-			.anyRequest().authenticated()
-			.and().httpBasic();
-	}
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/login", "/register").permitAll()
+                .anyRequest().authenticated()
+                .and().httpBasic();
+    }
 
-
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("Krishna").password("12345").authorities("User")
+                .and()
+                .withUser("Ankit").password("12345").authorities("admin")
+                .and()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
 }
