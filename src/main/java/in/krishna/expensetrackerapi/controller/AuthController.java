226bc051514b77bee56f3b5ad70f305294f3aba0
krnand <1india.kng@gmail.com>
2024-04-01 10:59:10 +0530

    jwt token is being generated

--- src/main/java/in/krishna/expensetrackerapi/controller/AuthController.java
+++ src/main/java/in/krishna/expensetrackerapi/controller/AuthController.java
@@ -1,17 +1,23 @@
 package in.krishna.expensetrackerapi.controller;
 
 import in.krishna.expensetrackerapi.entity.AuthModel;
+import in.krishna.expensetrackerapi.entity.JwtResponse;
 import in.krishna.expensetrackerapi.entity.User;
 import in.krishna.expensetrackerapi.entity.UserModel;
+import in.krishna.expensetrackerapi.security.CustomUserDetailsService;
 import in.krishna.expensetrackerapi.service.UserService;
+import in.krishna.expensetrackerapi.utils.JwtTokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,12 +33,33 @@ public class AuthController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
     @PostMapping("/login")
-    public ResponseEntity<HttpStatus> login(@RequestBody AuthModel authModel) {
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authModel.getEmail(),
-                authModel.getPassword()));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<JwtResponse> login(@RequestBody AuthModel authModel) throws Exception {
+        // Authenticate the user
+        authenticate(authModel.getEmail(),authModel.getPassword());
+        final UserDetails userDetails = customUserDetailsService.loadUserByUsername(authModel.getEmail());
+
+        final String token = jwtTokenUtil.generateToken(userDetails);
+
+        //we need to generate the Jwt token
+        return new ResponseEntity<JwtResponse>(new JwtResponse(token),HttpStatus.OK);
+    }
+
+    private void authenticate(String email, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email,
+                    password));
+        }catch (DisabledException e){
+            throw new Exception("User Disabled");
+        }catch (BadCredentialsException e){
+            throw new Exception("Bad Credential");
+        }
     }
 
     @PostMapping("/register")
